faeabb69b5e948f179ba38c320b29fd9e5e3a5fe
Yaydy <107906477+yaydy@users.noreply.github.com>
2022-10-11 21:29:21 +0000

    comments

--- .ccls-cache/@home@runner@SlopeFinder2Equations/main.c
+++ .ccls-cache/@home@runner@SlopeFinder2Equations/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+// i was told to comment my code soo...
 
 int main(void) {
+  // declare all terms used in formula
   int y[2], x[2], rise, run;
   for(int i = 0; i < 2; i++){
     printf("Put what Y%d is ", i + 1);
@@ -10,10 +12,12 @@ int main(void) {
     printf("Put what X%d is ", i + 1);
     scanf("%d", &x[i]);
   }
+  // do mathings
   rise = x[1] - x[0];
   run = y[1] - y[0];
   printf("m = %d/%d\n", rise, run);
   printf("Simplifying...\n");
+  // uses modulo operations to see if the answer can be simplfied
   while(run % 2 == 0 && rise % 2 == 0){
     run = run/2;
     rise = rise/2;
